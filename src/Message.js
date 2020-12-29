0e5638f40d304e40fbec2838e7d6c03d9119abd3
tkim294 <tkim294@wisc.edu>
2020-12-29 13:39:21 -0800

    sender and receiver differences

--- src/Message.js
+++ src/Message.js
@@ -1,13 +1,16 @@
 import { Avatar } from '@material-ui/core';
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectUser } from './features/userSlice';
 import './Message.css';
 
 const Message = ({id, contents: {
     timestamp, displayName, email, message, photo, uid
 }}) => {
+    const user = useSelector(selectUser);
     return (
-        <div className="message">
-            <Avatar src={photo} />
+        <div className={`message ${user.email === email && "message__sender"}`}>
+            <Avatar className="message__photo" src={photo} />
             <p>{message}</p>
             <small>{new Date(timestamp?.toDate()).toLocaleString()}</small>
         </div>
